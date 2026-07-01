73551fb80e8a9d8aa6225cc6be24d870eba71b61
Batul <batuljabalpurwala05@gmail.com>
2026-07-01 10:54:07 +0530

    Array using zeros and ones

---
+++ NumpyZeros.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr1 = np.zeros(8)
+arr2 = np.ones((4,4))
+arr3 = np.zeros((3,3))
+
+print("A 1D array of 8 zeros :",arr1)
+print("A 2D array of shape (4, 4) filled with ones :",arr2)
+print("A 3x3 matrix of zeros :",arr3)
